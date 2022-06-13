b3a5431d9d29267041af957df97124b1634eec6e
NamDuong2 <75520765+NamDuong2@users.noreply.github.com>
2022-06-13 12:30:14 +0700

    Update settings.py

--- TwitterAPI-Stream-Python/settings.py
+++ TwitterAPI-Stream-Python/settings.py
@@ -1,1 +1,9 @@
+TRACK_TERMS = ["#BTC", "#ETH", "#SOL", "#BNB", "#LUNA", "#NFT"]  # you can change track key words Example: Bitcoin - #BTC, or ethereum - #ETH
+KAFKA_TOPIC_IN = "tweetStreaming"    # twitter API to Kafka
+KAFKA_TOPIC_OUT = "visualize-tweet"    # Flink to Kafka
+BOOTSTRAP_SERVERS = "<ip-kafka-host>:<port>"  # <ip kafka host>:<port> in my case, my kafka container host on Ubuntu VM so i use my IP address Ubuntu VM
+try:
+    from private import *
+except Exception:
+    pass
 
