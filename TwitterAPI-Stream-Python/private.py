56b17b2aa09abe86698c8ebbfe71c942e81a5a1e
NamDuong2 <75520765+NamDuong2@users.noreply.github.com>
2022-06-13 12:32:22 +0700

    Create private.py

---
+++ TwitterAPI-Stream-Python/private.py
@@ -0,0 +1,6 @@
+# Twitter API v2 credentials
+# Sign up at https://developer.twitter.com/en/docs/twitter-api
+ACCESS_TOKEN = "your-access-token"
+ACCESS_TOKEN_SECRET = "your-access-token-secret"
+API_KEY = "your-api-key"
+API_KEY_SECRET = "your-api-key-secret"
